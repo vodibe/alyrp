e99a9ad5c5ec5cba7f8794b3093bc798ceba32b7
vodibe <v.dibisceglie.uni@gmail.com>
2022-09-24 11:05:19 +0200

    added buttons pinout constants (see more)
    added comments for user customization

--- include/alyrp.h
+++ include/alyrp.h
@@ -5,41 +5,55 @@
     #include "Arduino.h"
 #endif
 
-#include <SPI.h>
+//#include <SPI.h>
 #include <SdFat.h>
 #include <Regexp.h>
+#include <Bounce2.h>
+
+//--- COMMENT IF I2C MODULE HAS NOT BEEN INSTALLED ---
+#define LCD_I2C_EXISTS
+
+#ifdef LCD_I2C_EXISTS
+    #include <LiquidCrystal_I2C.h>
+    //--- SET I2C MODULE ADDRESS --- 
+    //i2c scanner at /test/i2c-scanner.c
+    #define LCD_I2C_ADDR 0x27
+#else
+    //note that <LiquidCrystal.h> refers to /lib/LiquidCrystal library
+    #include <LiquidCrystal.h>
+    //--- SET LCD PINOUT AT alyrp.cpp ---
+#endif
+
+// LCD constants
+//--- SET YOUR LCD DISPLAY ROWS AND COLS NUMBER ---
+#define LCD_ROWS 4
+#define LCD_COLS 20
 
 // SD chip select pin
+//--- SET YOUR SD CHIP SELECT PIN ---
 #define SD_CS_PIN 10
+
 // File name max length
 #define FILE_NAME_MAX_LENGTH 60
 // Name of directory containing all the lrc files
 #define LRC_DIR "/lyrics/"
-// lrc file extension
-#define LRC_EXT "lrc"
-// Given a timestamp such as [xx:yy.zz], calc its length
+// Given a timestamp such as [xx:yy.zz], 10 is its length
 #define LRC_TIMESTAMP_LENGTH 10
 
-// >=1 if a i2c module has been used, 0 otherwise
-#define LCD_I2C_EXISTS 0
-#if LCD_I2C_EXISTS >= 1
-    #define LCD_I2C_USED
-    #include <LiquidCrystal_I2C.h>
-#else
-    #include <LiquidCrystal.h>
-#endif
-
-#define LCD_ROWS 4
-#define LCD_COLS 20
-
-// LCD navigation constants (editable by the user)
+// LCD navigation constants
 #define LCD_NAV_NEXT 'x'
 #define LCD_NAV_OK 'c'
-
-// LCD navigation constants (NOT editable by the user)
 #define LCD_NAV_NEXT_OK 'k'
 
-// Functions prototypes
+// Buttons pinout
+// --- SET BUTTONS PINS ---
+#define BTN1_PIN 9
+#define BTN2_PIN 2
+
+// Navigation buttons constants
+#define DEBOUNCE_DELAY 50
+
+// Prototypes
 void browseDir(const char inByte);
 bool openPrevious(SdBaseFile &folder, SdFile &file, uint8_t oflag);
 
